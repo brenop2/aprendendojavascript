66d6c495ac3794386b6d287322aa36d46cbb70e8
brenop2 <breno.p231@gmail.com>
2025-02-16 00:45:19 -0300

    first commit

---
+++ Javaex01/aula11/ex1.js
@@ -0,0 +1,9 @@
+var velocidade = 40
+
+if (velocidade > 50) {
+    console.log('Você ta rapido para um Karalho seu filha da puta')
+} else {
+    console.log('Você esta muuito devagar filha da puta')
+}
+
+// node.js ou seja o que estou fazendo agora é sincronico
